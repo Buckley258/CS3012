7c02260987e7dfd2c2a8f2d8a633aafc996647e7
Buckley258 <mauricebuckley@hotmail.com>
2017-10-17 17:39:51 +0100

    first commit, basic class with test class made but no tests yet

---
+++ LowestCommonAncestor/TreeNode.java
@@ -0,0 +1,24 @@
+
+public class TreeNode {
+
+	public int val;
+	public TreeNode left = null;
+	public TreeNode right = null;
+
+	public TreeNode(int val) {
+		this.val = val;
+	}
+
+	public int getVal() {
+		return val;
+	}
+
+	public TreeNode getLeftNode() {
+		return left;
+	}
+
+	public TreeNode getRightNode() {
+		return right;
+	}
+
+}
