8f6b6473a3510b07378cc5ca4647782ec90eef6d
Buckley258 <mauricebuckley@hotmail.com>
2017-10-18 19:19:15 +0100

    finished test for delete

--- LowestCommonAncestor/LowestCommonAncestorTest.java
+++ LowestCommonAncestor/LowestCommonAncestorTest.java
@@ -26,6 +26,31 @@ class LowestCommonAncestorTest {
 	@Test
 	public void deleteTest() {
 		
+		TreeNode<Integer,Integer> tree = new TreeNode<Integer,Integer>();
+		//empty treenode
+		tree.delete(10);
+		assertEquals("()",tree.printTree());
+		tree.insert(10, 10);
+		tree.insert(8, 8);
+		tree.insert(15,15);
+		tree.insert(9,9);
+		tree.insert(5,5);
+		tree.insert(20,20);
+		tree.insert(2,2);
+		tree.insert(6,6);
+		tree.insert(12, 12);
+		//delete a leaf from the tree
+		tree.delete(20);
+		assertEquals("((((()2())5(()6()))8(()9()))10((()12())15()))",tree.printTree());
+		//delete leaf not in tree
+		tree.delete(21);
+		assertEquals("((((()2())5(()6()))8(()9()))10((()12())15()))",tree.printTree());
+		//delete leaf with a left node
+		tree.delete(15);
+		assertEquals("((((()2())5(()6()))8(()9()))10(()12()))",tree.printTree());
+		//delete leaf with a right node
+		tree.delete(6);
+		assertEquals("((((()2())5())8(()9()))10(()12()))",tree.printTree());
 		
 		
 	}
