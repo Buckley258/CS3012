354db88ace5963c9f77756b14aa0ae06abb91110
Buckley258 <mauricebuckley@hotmail.com>
2017-10-19 11:41:34 +0100

    finished task1 code

--- LowestCommonAncestor/LowestCommonAncestorTest.java
+++ LowestCommonAncestor/LowestCommonAncestorTest.java
@@ -74,6 +74,42 @@ class LowestCommonAncestorTest {
 		
 	}
 	
+	@Test
+	public void LCA_test() {
+		
+		TreeNode<Integer,Integer> tree = new TreeNode<Integer,Integer>();
+		//test on an empty treenode
+		assertNull(tree.lowestCommonAncestor(10, 15));
+		
+		tree.insert(10, 10);
+		//test correct nodes on single tree
+		assertEquals(10,(int)tree.lowestCommonAncestor(10, 10));
+		//test no correct nodes on single node tree
+		assertNull(tree.lowestCommonAncestor(15, 20));
+		//test 1 correct node on single node tree
+		assertNull(tree.lowestCommonAncestor(10, 15));
+		
+		tree.insert(6, 6);
+		tree.insert(7, 7);
+		tree.insert(9, 9);
+		tree.insert(3, 3);
+		tree.insert(5, 5);
+		tree.insert(1, 1);
+		tree.insert(15, 15);
+		tree.insert(20, 20);
+		
+		//multiple tests on normal many node tree
+		assertEquals(6, (int)tree.lowestCommonAncestor(1, 9));
+		assertEquals(3, (int)tree.lowestCommonAncestor(1, 5));
+		assertEquals(10, (int)tree.lowestCommonAncestor(20, 9));
+		assertEquals(10, (int)tree.lowestCommonAncestor(5, 20));
+		assertEquals(10, (int)tree.lowestCommonAncestor(6, 15));
+		//if one of the nodes is the root, return root
+		assertEquals(10, (int)tree.lowestCommonAncestor(10, 9));
+		
+		
+	}
+	
 	
 
 
