09ad152faa997a48c10a160609a7de3a7ca64fe3
Buckley258 <mauricebuckley@hotmail.com>
2017-11-24 11:05:06 +0000

    gather information on downloads of my second year project

--- APITask/src/API.java
+++ APITask/src/API.java
@@ -4,11 +4,12 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Arrays;
 
+// collect information on my software engineering project in second year
 public class API {
 	public static void main(String[] args) throws Exception {
 		
 		//Create HttpURLConnection 
-		HttpURLConnection httpcon = (HttpURLConnection) new URL("https://api.github.com/repos/goxr3plus/XR3Player/releases").openConnection();
+		HttpURLConnection httpcon = (HttpURLConnection) new URL("https://api.github.com/repos/Seanie96/KinectMultiplayerGame/releases").openConnection();
 		httpcon.addRequestProperty("User-Agent", "Mozilla/5.0");
 		BufferedReader in = new BufferedReader(new InputStreamReader(httpcon.getInputStream()));
 		
@@ -21,7 +22,7 @@ public class API {
 		}
 		in.close();
 		
-		//Get Git Hub Downloads of XR3Player
+		//Get Git Hub Downloads of KinectMultiplayerGame
 		Arrays.stream(responseSB.toString().split("\"download_count\":")).skip(1).map(l -> l.split(",")[0]).forEach(l -> System.out.println(l));
 		
 		//Sum up all download counts
